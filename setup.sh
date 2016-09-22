54ec6a82c0b21b1eb11baed1551b296e0a420d5d
Erik Donohoo <erikdonohoo@gmail.com>
2016-09-21 19:42:20 -0600

    start work
    continue on consolidating .bash_profile setup

--- setup.sh
+++ setup.sh
@@ -12,92 +12,93 @@ echo "Updating Brew"
 brew update
 
 #Bash profile
-nano ~/.bash_profile
-export PATH=/usr/local/bin:$PATH
+nano ~/.bashrc
+alias ll="ls -al"
+export PATH=$HOME/local/bin:$PATH
+alias git-branch-clean="git branch --merged | grep -v \"\*\" | grep -v master | grep -v develop | xargs -n 1 git branch -d"
+alias git-stash-diff="git stash show -p stash@{0}"
+alias vbox-edge-box-cam-attach="VBoxManage controlvm \"IE11 - Win10\" webcam attach .1"
+alias vbox-edge-box-cam-detach="VBoxManage controlvm \"IE11 - Win10\" webcam detach .1"
+###-begin-npm-completion-###
+#
+# npm command completion script
+#
+# Installation: npm completion >> ~/.bashrc  (or ~/.zshrc)
+# Or, maybe: npm completion > /usr/local/etc/bash_completion.d/npm
+#
+
+if type complete &>/dev/null; then
+  _npm_completion () {
+    local words cword
+    if type _get_comp_words_by_ref &>/dev/null; then
+      _get_comp_words_by_ref -n = -n @ -w words -i cword
+    else
+      cword="$COMP_CWORD"
+      words=("${COMP_WORDS[@]}")
+    fi
+
+    local si="$IFS"
+    IFS=$'\n' COMPREPLY=($(COMP_CWORD="$cword" \
+                           COMP_LINE="$COMP_LINE" \
+                           COMP_POINT="$COMP_POINT" \
+                           npm completion -- "${words[@]}" \
+                           2>/dev/null)) || return $?
+    IFS="$si"
+  }
+  complete -o default -F _npm_completion npm
+elif type compdef &>/dev/null; then
+  _npm_completion() {
+    local si=$IFS
+    compadd -- $(COMP_CWORD=$((CURRENT-1)) \
+                 COMP_LINE=$BUFFER \
+                 COMP_POINT=0 \
+                 npm completion -- "${words[@]}" \
+                 2>/dev/null)
+    IFS=$si
+  }
+  compdef _npm_completion npm
+elif type compctl &>/dev/null; then
+  _npm_completion () {
+    local cword line point words si
+    read -Ac words
+    read -cn cword
+    let cword-=1
+    read -l line
+    read -ln point
+    si="$IFS"
+    IFS=$'\n' reply=($(COMP_CWORD="$cword" \
+                       COMP_LINE="$line" \
+                       COMP_POINT="$point" \
+                       npm completion -- "${words[@]}" \
+                       2>/dev/null)) || return $?
+    IFS="$si"
+  }
+  compctl -K _npm_completion npm
+fi
+###-end-npm-completion-###
 source ~/.bash_profile
 
 echo "Installing Cask..."
 brew install caskroom/cask/brew-cask
 brew upgrade brew-cask && brew cleanup && brew cask cleanup
-
-# VirtualEnv & Python Setup
-export PATH=/usr/local/lib/python2.7/site-packages:$PATH
 source ~/.bash_profile
-sudo easy_install pip
-sudo pip install virtualenv virtualenvwrapper
-nano ~/.bash_profile
-source /usr/local/bin/virtualenvwrapper_lazy.sh
-less ~/.bash_profile
-sudo pip install numpy
 
+echo "Install apps"
 brew cask install google-chrome
-brew cask install dropbox
 brew cask install google-drive
-brew cask install evernote
-brew cask install handbrake
-brew cask install kindle
-brew cask install things
-#brew cask install lorem
-brew cask install papers
-brew cask install sketchup
-brew cask install spotify
+brew cash install virtualbox
+brew cask install sequel-pro
+brew cask install atom
+brew cask install slack
 
-brew tap homebrew/science
 brew install gcc
-brew install Caskroom/cask/xquartz
-brew install r
-brew install libssh2
-
-
-#brew cask install R
-brew cask install rstudio
-brew install jags
-
-echo "Installing R Packages"
-$ R
-	install.packages("runjags")
-	install.packages("coda")
-	install.packages("dplyr")
-$q()
-$n
-
-brew cask install sublimetext
-	#Install R Package Control https://packagecontrol.io/installation
-	#Install R-Box
-brew cask install virtualbox
-
-# brew cask install x11vnc ??
-brew install git
 brew install bash-git-prompt
 brew install git bash-completion
 
-brew install node
-#brew install zsh
-export NODE_PATH=/usr/local/lib/node_modules
-brew install npm
-brew cask install sequel-pro
-brew cask install webstorm
-brew cask install intellij-ce
-brew cask install atom
-	# atom-beautify
-	# editorconfig
-	# file-icons
-	# language-r
-	# linter
-	# linter-jscs
-	# linter-jshint
-	# merge-conflicts
-	# minimap
-	# repl-term
-	# tab-to-spaces
+echo "Install nvm"
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+echo "source ~/.bashrc" >> ~/.bash_profile
+source ~/.bash_profile
 
-#Install Browser-sycn and Gulp
-npm install browser-sync gulp --save-dev
-
-# Microsoft Office
-# Adobe CC
-# Adobe CS6 Illustrator and InDesign
-# things
-# Sequel pro
-# TextWrangler
-# VPN Client
+echo "Install atom plugins"
+apm i atom-beautify editorconfig file-icons linter linter-jscs linter-jshint merge-conflicts minimap tabs-to-spaces pigments tag jscs-fixer linter-jsonlint atom-typescript
